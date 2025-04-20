63ec100292d0d8f1800f15181c8ada1eb18992a1
Olfa <olfa.medimegh@insat.ucar.tn>
2025-04-20 19:38:03 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello depuis Docker !!');
+  res.send('Hello depuis Docker !');
 });
 
 app.listen(3000, '0.0.0.0', () => {
