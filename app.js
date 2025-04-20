824f549142e4d6e85a4808bd3208b68289fc4404
Olfa <olfamedimegh@gmail.com>
2025-04-20 15:09:24 +0100

    Add bode js app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello depuis Docker !');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Serveur lancé sur le port 3000');
+});
